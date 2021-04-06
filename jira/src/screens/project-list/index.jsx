5f46711d5376f54087251b56593e7681211ebd8e
曾丹 <>
2021-04-06 19:37:32 +0800

    message

--- jira/src/screens/project-list/index.jsx
+++ jira/src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import react, { useState, useEffect } from 'react';
 import * as qs from 'qs';
 import { SearchPanel } from './search-panel';
 import { List } from './list';
-import { cleanObject } from 'utils';
+import { cleanObject, useMount, useDebounce } from 'utils';
 
 const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () => {
@@ -12,24 +12,27 @@ export const ProjectListScreen = () => {
   });
   const [users, setUsers] = useState([]);
   const [list, setList] = useState([]);
+  const debounceParam = useDebounce(param, 1000);
+
   //name=${param.name}&personId=${param.personId}
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
-      async (response) => {
-        if (response.ok) {
-          setList(await response.json());
-          console.log(response);
-        }
+    // console.log(debounceParam);
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
       }
-    );
-  }, [param]);
-  useEffect(() => {
+    });
+  }, [debounceParam]);
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json());
       }
     });
-  }, []);
+  });
+
   return (
     <div>
       <SearchPanel
--- jira/src/utils/index.js
+++ jira/src/utils/index.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 //需求：将对象中值为空的键删除
 //在一个函数中，改变传入的对象本身是不好的
 export const isFalsy = (value) => value === 0 ? false : !value;
@@ -10,4 +11,25 @@ export const cleanObject = (object) => {
     }
   })
   return result
+}
+
+//Custom Hook
+//1、组件加载的时候只执行一次
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback()
+  }, [])
+}
+
+//防抖hook
+//什么是hook的先天优势？
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+  //每次在value变化以后，都设置一个定时器
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebounceValue(value), delay);
+    //每次在上一个useEffect处理完再运行
+    return () => { clearTimeout(timeout) };
+  }, [value, delay])
+  return debounceValue;
 }
