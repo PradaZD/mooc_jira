8cea219c38c643cf0f4325abacc35f7820c1809d
曾丹 <>
2021-07-01 10:11:18 +0800

    ts了解

--- jira/src/App.tsx
+++ jira/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import { ProjectList } from './screens/project-list'
+import { TsReactTest} from './try-use-array'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <ProjectList />
+      {/* <ProjectList /> */}
+      <TsReactTest/>
     </div>
 
   );
--- jira/src/utils/index.ts
+++ jira/src/utils/index.ts
@@ -1,10 +1,10 @@
 import { AnyARecord } from 'dns'
 import { useState, useEffect } from "react"
 
-export const isFalsy = (value:any) => value === 0 ? false : !value
+export const isFalsy = (value: unknown) => value === 0 ? false : !value
 //URL中的参数可能存在值是无效值（比如null,undefined,'',NAN这些），此时我们需要做判断
 //如果有参数属性值是无效值，那么就把这个参数属性名删除，可以避免无效搜索
-export const clearObject = (obj:object) => {
+export const clearObject = (obj: object) => {
   // const result=Object.assign({},obj)
   const result = { ...obj }
   Object.keys(result).forEach(key => {
@@ -19,14 +19,15 @@ export const clearObject = (obj:object) => {
   return result
 }
 //自定义属性，处理页面加载完成后只执行一次的函数
-export const useMount = (callback:()=>void) => {
+export const useMount = (callback: () => void) => {
   // hooks只能在组件或者其他hooks中运行，不能在普通函数中运行
   useEffect(() => {
     callback()
   }, [])
 }
 //防抖自定义hooks
-export const useDebounce = (value:any, delay?:number) => {
+// 后面用泛型来规范类型
+export const useDebounce = <T>(value: T, delay?: number): T => {
   const [debounceValue, setDebounceValue] = useState(value)
   useEffect(() => {
     const timeout = setTimeout(() => setDebounceValue(value), delay)
@@ -34,4 +35,22 @@ export const useDebounce = (value:any, delay?:number) => {
     return () => clearTimeout(timeout)
   }, [value, delay])
   return debounceValue
+}
+
+export const useArray = <T>(initArray: T[]) => {
+  const [value, setValue] = useState(initArray)
+  return {
+    value,
+    setValue,
+    clear: () => setValue([]),
+    removeIndex: (index: number) => {
+      const copyVal = [...value]
+      copyVal.splice(index, 1)
+      setValue(copyVal)
+    },
+    add: (item:T) => {
+      setValue([...value,item])
+    }
+  }
+
 }
